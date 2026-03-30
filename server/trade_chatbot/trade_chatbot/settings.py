ad667caff96854c42c6bb1302b6044465c870859
khannaveed2310 <khan23naveed@gmail.com>
2026-03-30 22:46:18 +0530

    allowed host add in setting

--- server/trade_chatbot/trade_chatbot/settings.py
+++ server/trade_chatbot/trade_chatbot/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
      'localhost',
     '127.0.0.1',
-    'tradepulse-7o0a.onrender.com',  
+    'tradepulse-6726.onrender.com',  
     '.onrender.com', 
     ".vercel.app",    
 ]
